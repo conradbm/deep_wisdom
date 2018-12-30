07b42caf72ac74f968c05f003dcc9f48a42880b6
conradbm <bmc.cs@outlook.com>
2018-12-30 15:29:25 -0600

    updates django working copy

--- deep_wisdom_django/dwsite/dwsite/urls.py
+++ deep_wisdom_django/dwsite/dwsite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+	path('',include('deepwisdom.urls')),
     path('deepwisdom/', include('deepwisdom.urls')),
     path('admin/', admin.site.urls),
 ]
